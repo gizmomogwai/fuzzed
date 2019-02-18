852b7b72c4b4bec65b82a7529390c1c66899039c
Christian Koestlin <christian.koestlin@esrlabs.com>
2019-02-18 01:43:59 +0100

    Install executable to ~/bin

--- rakefile.rb
+++ rakefile.rb
@@ -10,5 +10,8 @@ task :format do
     sh "find . -name '*.d' | xargs dfmt -i"
 end
 
-task :default => [:format, :test, :build]
+task :install do
+    sh "cp out/exe/fuzzed ~/bin/"
+end
+task :default => [:format, :test, :build, :install]
 
